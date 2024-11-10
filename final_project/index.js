b43cddf3ab927b9b518d702a5db7bb0505c99c78
Daniel <callejasmark63@gmail.com>
2024-11-10 14:25:26 +0800

    Lab Finish

--- final_project/index.js
+++ final_project/index.js
@@ -5,14 +5,26 @@ const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
-
+const SECRET_KEY = 'secret_key'
 app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
+const token = req.session.token; // Retrieve token from session
+
+    if (!token) {
+        return res.status(401).json({ message: "Access denied. No token provided." });
+    }
+
+    try {
+        // Verify the token
+        const decoded = jwt.verify(token, SECRET_KEY);
+        req.user = decoded; // Attach decoded payload to request
+        next();
+    } catch (err) {
+        res.status(403).json({ message: "Invalid token." });
+    }});
  
 const PORT =5000;
 
